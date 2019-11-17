47a71899015c3ca63e50243f825c95481cfc9a5f
Safarali <mr.safarali@gmail.com>
2019-11-16 21:07:55 -0700

    Add validation and sanitization to Models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -21,11 +21,38 @@ const User = mongoose.model('User', {
         type: String,
         required: [true, 'This field cannot be empty'],
         minlength: [true, 'Name must contain at least 3 characters'],
+        trim: true,
     },
     email: {
         type: String,
         required: [true, 'Email field cannot be empty'],
         validate: [validator.isEmail, 'Please provide a valid email'],
+        unique: true,
+        lovercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Please provide a password'],
+        minlength: [8, 'Password must contain at least 8 characters'],
+        trim: true,
+        validate: {
+            validator: function() {
+                return !this.password.toLowerCase().includes('password');
+            },
+            message: 'Password must not contain a word password',
+        },
+    },
+
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please provide a password confirmation'],
+        validate: {
+            validator: function(passwordConfirm) {
+                return passwordConfirm === this.password;
+            },
+            message: 'Password confirmation does not match the password',
+        },
     },
 });
 
@@ -35,6 +62,7 @@ const Task = mongoose.model('Task', {
         required: [true, 'This field cannot be empty'],
         minlength: [10, 'Todo must contain at least 10 characters'],
         maxlength: [50, 'Todo must contain less than 50 characters'],
+        trim: true,
     },
     completed: {
         type: Boolean,
