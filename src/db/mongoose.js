4d1a4205325ecdd505657a6ee70e86dd76ce1eea
Safarali <mr.safarali@gmail.com>
2019-11-16 20:28:41 -0700

    Creare Task and User model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const validator = require('validator');
 
 dotenv.config({ path: './config.env' });
 const DB_URL = process.env.DB_URL.replace(
@@ -14,3 +15,29 @@ mongoose
         useFindAndModify: false,
     })
     .then(() => console.log('DB connection is successful'));
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: [true, 'This field cannot be empty'],
+        minlength: [true, 'Name must contain at least 3 characters'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email field cannot be empty'],
+        validate: [validator.isEmail, 'Please provide a valid email'],
+    },
+});
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: [true, 'This field cannot be empty'],
+        minlength: [10, 'Todo must contain at least 10 characters'],
+        maxlength: [50, 'Todo must contain less than 50 characters'],
+    },
+    completed: {
+        type: Boolean,
+        default: false,
+    },
+});
