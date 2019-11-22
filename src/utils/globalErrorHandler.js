c9cd10f03094d2ea719630655ffb2b25ce7911ca
Safarali <mr.safarali@gmail.com>
2019-11-22 11:35:30 -0700

    Handle Errors

---
+++ src/utils/globalErrorHandler.js
@@ -0,0 +1,82 @@
+const AppError = require('./../utils/appError');
+
+// Global Error Handler
+const handleCastErrorDB = (err) => {
+    const message = `Invalid ${err.path}: ${err.value}`;
+    return new AppError(message, 400);
+};
+
+const handleDublicateFieldsDB = (err) => {
+    const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+    const message = `Dublicate field value: ${value}. Please use another value`;
+    return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map((error) => error.message);
+    const message = `Invalid data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+    new AppError('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () =>
+    new AppError('Your token has expired!. Please login again', 401);
+
+const sendErrorDev = (err, req, res) => {
+    // API
+    if (req.originalUrl.startsWith('/api')) {
+        return res.status(err.statusCode).json({
+            status: err.status,
+            error: err,
+            message: err.message,
+            stack: err.stack,
+        });
+    }
+    console.log('Error: 🛠 ', err);
+};
+
+const sendErrorProd = (err, req, res) => {
+    // Operational, trusted error: send message to client
+    // Else, Programming or other unknown error: dont leak error details
+
+    // API
+    if (req.originalUrl.startsWith('/api')) {
+        // Operational, trusted error
+        if (err.isOperational) {
+            return res.status(err.statusCode).json({
+                status: err.status,
+                message: err.message,
+            });
+        }
+
+        // Unknown or Programming error
+        // 1) Log error
+        console.log('Error: 🛠 ', err);
+
+        // 2) Send generic message
+        return res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong',
+        });
+    }
+};
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, req, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err, message: err.message };
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        if (error.code === 11000) error = handleDublicateFieldsDB(error);
+        if (error.name === 'ValidationError')
+            error = handleValidationErrorDB(error);
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+        sendErrorProd(error, req, res);
+    }
+};
